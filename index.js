7d16af81237aa7c4d13f0c92fe55604fae823310
cdartora <cdartora>
2021-10-26 16:17:10 -0300

    estiliza header e atualiza logo

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const title = document.querySelector('.book-name');
 const cover = document.querySelector('.book-cover');
 
 const fetchBooks = async (endpoint) => {
-  const url = `https://www.googleapis.com/books/v1/volumes?q=${endpoint}`;
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${endpoint}+subject`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data.items[0].volumeInfo);
+  console.log(data.items);
 
-  title.innerText = data.items[0].volumeInfo.title;
-  cover.src = data.items[0].volumeInfo.imageLinks.thumbnail;
+  title.innerText = data.items[1].volumeInfo.title;
+  cover.src = data.items[1].volumeInfo.imageLinks.thumbnail;
 };
 
-fetchBooks('machado de assis');
+fetchBooks('ficção científica');
