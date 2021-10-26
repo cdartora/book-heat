c614eec965dbc1b41c317ffbf5628698e489f442
Kazuo Abduch <kazuo.abduch@gmail.com>
2021-10-26 16:50:47 -0300

    Add randomNumber function

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const title = document.querySelector('.book-name');
 const cover = document.querySelector('.book-cover');
 
+const generateRandomNumber = (length) => Math.floor(Math.random()*length);
+
 const fetchBooks = async (endpoint) => {
-  const url = `https://www.googleapis.com/books/v1/volumes?q=${endpoint}`;
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${endpoint}&maxResults=40`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data.items[0].volumeInfo);
+  console.log(data.items.length);
+  const randomNumber = generateRandomNumber(data.items.length);
 
-  title.innerText = data.items[0].volumeInfo.title;
-  cover.src = data.items[0].volumeInfo.imageLinks.thumbnail;
+  title.innerText = data.items[randomNumber].volumeInfo.title;
+  cover.src = data.items[randomNumber].volumeInfo.imageLinks.thumbnail;
 };
 
-fetchBooks('machado de assis');
+window.onload = () => {
+  fetchBooks('machado de assis');
+}
