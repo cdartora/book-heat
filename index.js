b340e8ee116cd65349c025885197d189dcaf0cdc
Kazuo Abduch <kazuo.abduch@gmail.com>
2021-10-26 17:54:55 -0300

    Search withour filter

--- index.js
+++ index.js
@@ -69,6 +69,9 @@ const searchButtonEvent = () => {
   } else if (searchBar.includes('byAuthor')) {
     createLoadscreen();
     fetchBooks(searchByAuthor(input.value));
+  } else {
+    createLoadscreen();
+    fetchBooks(input.value)
   }
 }
 
